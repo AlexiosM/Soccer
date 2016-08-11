6042e95ca2b971dfefb86a140ee19f267a8068f4
Alex <balex@alex.gr>
2016-08-11 18:16:26 -0400

    adding Player::movement function

--- soccer_game.cpp
+++ soccer_game.cpp
@@ -1,5 +1,27 @@
 #include"soccer_game.h"
 
+#define X_cells 5
+#define Y_cells 9
+
+void Player::movement(){ // change player column
+    if(player_position.x_position == X_cells){
+        player_position.x_position--;
+        return;
+    }
+    else if (player_position.x_position == 0){
+        player_position.x_position++;
+        return;
+    }
+    else {
+        srand(time(NULL)); 
+        player_position.x_position = ((rand()%100) < 50)?(player_position.x_position+1):(player_position.x_position-1);
+    }
+}
+
+void Player::pass(){
+
+}
+
 int main()
 {
     return 0;
--- soccer_game.h
+++ soccer_game.h
@@ -11,20 +11,21 @@ typedef struct {
     uint8_t y_position;
 }position_t;
 
-
 class Player
 {
-public:
-    void movement();
-    void pass();
-    virtual void specialMovement() = 0; // in order Player to be an abstract class
-
 private:
     string player_name;
     uint8_t shirt_number;
     position_t player_position;
     uint8_t movement_line;
     uint8_t target_line;
+
+public:
+    Player();
+    ~Player();
+    void movement();
+    void pass();
+    virtual void specialMovement() = 0; // in order Player to be an abstract class
 };
 
 class Team
@@ -36,17 +37,35 @@ private:
     uint8_t goals;
     uint16_t passings;
 
+public:
+    Team();
+    ~Team();
+    void addPlayer(Player &p);
+
+// setters and getters
+    uint8_t getMistakes();
+    uint8_t getGoals();
+    uint16_t getPassings();
 };
 
-class Fullback : public Player
+class Defensive : public Player
 {
 private:
    bool yellow_card;
    uint8_t steals; // shared to all objects
+
+public:
+    Defensive();
+    ~Defensive(); 
 };
 
 class Attacker : public Player
 {
+
+
+public:
+    Attacker();
+    ~Attacker();
 
 };
 
@@ -61,6 +80,7 @@ public:
     current_player = p1;
     ex_player = p2;
     }
+    ~Ball();
     void action();
     void assing(Player &p);
 };
@@ -69,10 +89,12 @@ class Game
 {
 private:
     uint8_t turns;
-    Ball ball;
-    Team team_a;
-    Team team_b;
+    Ball *ball;
+    Team *team_a;
+    Team *team_b;
 
 public:
+    Game();
+    ~Game();
     void runTurn(); // calls action for each team with random order
 };
